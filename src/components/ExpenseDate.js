c2f84e65e1a5ea892b5862aa2b639cf4dd86b2bf
sandeep <sandeep.kmr.kmr@gmail.com>
2022-10-12 13:54:13 -0700

    NEWEXPENSE DATE file

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ExpenseDate = (date) => {
+    const month=date.date.toLocaleString('en-US',{month:'long'});
+    const day=date.date.toLocaleString('en-US',{ day:'2-digit'});
+    const year =date.date.getFullYear();
+  return (
+    <div> 
+    <div >{month}</div>
+    <div >{year}</div>
+    <div >{day}</div>
+    </div>
+  )
+}
+
+export default ExpenseDate
