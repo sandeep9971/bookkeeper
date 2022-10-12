e2d8b42accb4ec043e977480b5bf4503d8c4273d
sandeep <sandeep.kmr.kmr@gmail.com>
2022-10-12 14:02:07 -0700

    some reference for import

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import 'Expensedate'
+import './ExpenseDate.css'
 const ExpenseDate = (date) => {
     const month=date.date.toLocaleString('en-US',{month:'long'});
     const day=date.date.toLocaleString('en-US',{ day:'2-digit'});
     const year =date.date.getFullYear();
   return (
-    <div> 
-    <div >{month}</div>
-    <div >{year}</div>
-    <div >{day}</div>
+    <div className="expense-date"> 
+    <div className="expense-date__month">{month}</div>
+    <div className="expense-date__year">{year}</div>
+    <div className="expense-date__day">{day}</div>
     </div>
   )
 }
