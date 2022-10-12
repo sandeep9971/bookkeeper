3d521e73849d02068c0bec09cda912f916436556
sandeep <sandeep.kmr.kmr@gmail.com>
2022-10-12 13:57:51 -0700

    new expense date file component

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import 'Expensedate'
 const ExpenseDate = (date) => {
     const month=date.date.toLocaleString('en-US',{month:'long'});
     const day=date.date.toLocaleString('en-US',{ day:'2-digit'});
